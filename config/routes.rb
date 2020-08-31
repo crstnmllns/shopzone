fbae473afc613ce46ada819527e3b997024288a7
crstn <cristiancarrasco80@gmail.com>
2020-08-30 21:35:33 -0400

    integrate active storage in view products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users' }
   resources :categories
-  
   resources :products
   resources :prices
   resources :users
