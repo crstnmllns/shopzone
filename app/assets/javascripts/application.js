c910cfcef2d51273b9c1d223c2cced2561239f80
crstn <cristiancarrasco80@gmail.com>
2020-09-08 20:23:11 -0300

    add dependencies of yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require chartkick
 //= require highcharts
 //= require Chart.bundle
-//= require jquery/dist/jquery
 //= require_tree .
